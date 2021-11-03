da370b58e6e6db33bc718dd231b292cd3a2de758
Drevill95 <89485867+Drevill95@users.noreply.github.com>
2021-11-03 08:27:30 -0600

    Add files via upload

---
+++ help.js
@@ -0,0 +1,101 @@
+const help = (prefix) => {
+	return `╭❍ *𝐒𝐎𝐅𝐈•𝐁𝐎𝐓*
+│
+│Prefix : ⌜ ${prefix} ⌟
+│Creador : ConfuMods
+│
+│✎_𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔_
+│
+│๛ ${prefix}sticker
+│๛ ${prefix}attp texto
+│๛ ${prefix}emoji 
+│
+│✎_𝑪𝒐𝒏𝒇𝒖_
+│
+│๛ ${prefix}creador
+│
+│✎_𝑪𝒐𝒏𝒗𝒆𝒓𝒕𝒊𝒅𝒐𝒓𝒆𝒔_
+│
+│๛ ${prefix}toimg
+│๛ ${prefix}tomp3
+│
+│✎_𝑶𝒕𝒂𝒌𝒖_
+│
+│๛ ${prefix}loli
+│๛ ${prefix}waifu
+│๛ ${prefix}muslos
+│
+│✎_𝑨𝒖𝒅𝒊𝒐_
+│
+│๛ ${prefix}tts es texto
+│
+│✎_𝑫𝒆𝒔𝒄𝒂𝒓𝒈𝒂𝒔_
+│
+│๛ ${prefix}play name
+│๛ ${prefix}ytmp3 link
+│๛ ${prefix}ytmp4 link
+│
+│✎_𝑬𝒅𝒊𝒕𝒔_
+│
+│๛ ${prefix}box texto
+│๛ ${prefix}love texto
+│๛ ${prefix}love2 texto
+│๛ ${prefix}ice texto
+│๛ ${prefix}blood texto
+│๛ ${prefix}blood2 texto
+│๛ ${prefix}grose texto
+│๛ ${prefix}luxury texto
+│๛ ${prefix}bokeh texto
+│๛ ${prefix}toxic texto
+│
+│✎_𝑱𝒖𝒆𝒈𝒐𝒔_
+│
+│๛ ${prefix}charlie
+│๛ ${prefix}juego
+│๛ ${prefix}slot
+│
+│✎_𝑶𝒕𝒓𝒐𝒔_
+│
+│๛ ${prefix}wame
+│๛ ${prefix}frases
+│๛ ${prefix}piropos
+│
+│✎_𝑮𝒓𝒖𝒑𝒐_
+│
+│๛ ${prefix}cerrargp
+│๛ ${prefix}abrirgp
+│๛ ${prefix}kick @
+│๛ ${prefix}promote @
+│๛ ${prefix}listadmin
+│๛ ${prefix}hidetag texto
+│๛ ${prefix}linkgc
+│๛ ${prefix}tagall
+│๛ ${prefix}tagall2
+│๛ ${prefix}tagall3
+│๛ ${prefix}tagall4
+│๛ ${prefix}tagall5
+│๛ ${prefix}totag
+│๛ ${prefix}contag
+│๛ ${prefix}doctag
+│๛ ${prefix}giftag
+│๛ ${prefix}sticktag
+│๛ ${prefix}kick @
+│๛ ${prefix}salir
+│๛ ${prefix}welcome [1/0]
+│๛ ${prefix}antilink [1/0]
+│๛ ${prefix}nsfw [1/0]
+│๛ ${prefix}delete
+│๛ ${prefix}creadorgp
+│
+│✎_𝑵𝒔𝒇𝒘_
+│
+│๛ ${prefix}nsfwmenu
+│
+│✎_𝐂𝐫𝐞𝐝𝐢𝐭𝐨𝐬_
+│ Confu : https://youtube.com/c/confumods
+│ Confu : wa.me/+50254371795
+│ Confu : instagram.com/confumods
+╰❏`
+}
+
+exports.help = help
